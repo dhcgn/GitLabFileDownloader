0f886417f2c73e9b8bbd39d39878142c52df8832
Daniel <dhcgn@users.noreply.github.com>
2020-05-06 14:32:59 +0200

    Update _release.ps1

--- _release.ps1
+++ _release.ps1
@@ -5,13 +5,13 @@ function Get-ScriptDirectory {
 $equinox = (Resolve-Path (Join-Path (Get-ScriptDirectory) "build\tools\equinox.exe")).Path
 $key = (Resolve-Path (Join-Path (Get-ScriptDirectory) "build\secrets\equinox.key")).Path
 $token = Get-Content (Resolve-Path (Join-Path (Get-ScriptDirectory) "build\secrets\token.txt")).Path
-$version = "2.0.0"
+$version = "2.0.1"
 
 . $equinox release --version=$version `
-    --platforms="windows_amd64 linux_amd64" `
-    --channel "stable" `
+    --platforms="windows_amd64 windows_386 linux_amd64 linux_386" `
+    --channel "beta" `
     --signing-key=$key `
     --app="app_dTSgRj85fgP" `
     --token=$token `
     -- -ldflags ("-s -w -X main.version={0}" -f $version) -trimpath `
-    github.com/GitLabFileDownloader
+    github.com/dhcgn/GitLabFileDownloader/cmd/GitLabFileDownloader
